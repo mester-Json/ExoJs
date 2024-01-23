3bf407c0d7b7a4ad969def52a907bc2bfe0d9795
root <root@asusjayson.home>
2024-01-23 18:53:15 +0100

    Grosse Maj Trol

--- asset/js/sombre.js
+++ asset/js/sombre.js
@@ -3,21 +3,24 @@ document.getElementById('darkModeToggle').addEventListener('click', function () 
     const h1 = document.querySelectorAll('h1');
     const nav = document.querySelector('nav');
     const header = document.querySelector('header');
-    const footer = document.querySelector('footer');
     const form = document.querySelector('form');
     const a = document.querySelectorAll('a');
-    const table = document.querySelectorAll('table');
-    const inpunt = document.querySelectorAll('input');
+    const td = document.querySelector('td');
+    const thead = document.querySelector('thead');
+    const tbody = document.querySelector('tbody');
+    const input = document.querySelectorAll('input');
     const button = document.querySelectorAll('button');
     const label = document.querySelectorAll('label');
-
+    const p = document.querySelectorAll('p');
+    const footer = document.querySelector('footer');
 
     if (body.classList.contains('dark')) {
         body.classList.remove('dark');
         nav.classList.remove('dark');
         header.classList.remove('dark');
-        form.classList.remove('dark');
-        table.classList.remove('dark');
+        thead.classList.remove('dark');
+        tbody.classList.remove('dark');
+        td.classList.remove('dark');
         footer.classList.remove('dark');
         for (let i = 0; i < h1.length; i++) {
             h1[i].classList.remove('dark');
@@ -25,8 +28,8 @@ document.getElementById('darkModeToggle').addEventListener('click', function () 
         for (let i = 0; i < a.length; i++) {
             a[i].classList.remove('dark');
         }
-        for (let i = 0; i < inpunt.length; i++) {
-            inpunt[i].classList.remove('dark');
+        for (let i = 0; i < input.length; i++) {
+            input[i].classList.remove('dark');
         }
         for (let i = 0; i < button.length; i++) {
             button[i].classList.remove('dark');
@@ -34,21 +37,27 @@ document.getElementById('darkModeToggle').addEventListener('click', function () 
         for (let i = 0; i < label.length; i++) {
             label[i].classList.remove('dark');
         }
+        for (let i = 0; i < p.length; i++) {
+            p[i].classList.remove('dark');
+        }
+
     } else {
         body.classList.add('dark');
         nav.classList.add('dark');
-        footer.classList.add('dark');
         header.classList.add('dark');
-        form.classList.add('dark');
-        table.classList.remove('dark')
+        thead.classList.add('dark');
+        tbody.classList.add('dark');
+        td.classList.add('dark');
+        footer.classList.add('dark');
+
         for (let i = 0; i < h1.length; i++) {
             h1[i].classList.add('dark');
         }
         for (let i = 0; i < a.length; i++) {
             a[i].classList.add('dark');
         }
-        for (let i = 0; i < inpunt.length; i++) {
-            inpunt[i].classList.add('dark');
+        for (let i = 0; i < input.length; i++) {
+            input[i].classList.add('dark');
         }
         for (let i = 0; i < button.length; i++) {
             button[i].classList.add('dark');
@@ -56,6 +65,8 @@ document.getElementById('darkModeToggle').addEventListener('click', function () 
         for (let i = 0; i < label.length; i++) {
             label[i].classList.add('dark');
         }
+        for (let i = 0; i < p.length; i++) {
+            p[i].classList.add('dark');
+        }
     }
-
 });
--- asset/js/troll.js
+++ asset/js/troll.js
@@ -7,7 +7,7 @@ document.body.addEventListener('click', function () {
 
 document.body.addEventListener('keydown', function () {
     const couleurs = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-    const audio = new Audio("asset/audio/pump-shotgun-fortnite-loud.mp3");
+    const audio = new Audio("asset/audio/shotgun_1.mp3");
     audio.play();
     document.body.style.backgroundColor = couleurs[Math.floor(Math.random() * couleurs.length)];
 });
@@ -23,5 +23,26 @@ document.body.addEventListener('keydown', function (e) {
 document.body.addEventListener('keydown', function (e) {
     if (e.key === '5') {
         document.body.style.transform = 'rotate(0deg)';
+    }
+});
+
+
+const reload = document.getElementById('reload');
+
+reload.addEventListener('click', function () {
+    window.location.reload();
+});
+
+const darkModeToggle = document.getElementById('darkModeToggle');
+const formulaire = document.querySelector('.containerForm');
+let isHidden = false;
+
+darkModeToggle.addEventListener('click', function () {
+    if (isHidden) {
+        formulaire.style.display = 'block';
+        isHidden = false;
+    } else {
+        formulaire.style.display = 'none';
+        isHidden = true;
     }
 });
